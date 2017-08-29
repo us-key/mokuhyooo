0a06f06f88c7646f25acc7a4b17d94a1e5ba446f
us-key <y.omt0601@gmail.com>
2017-08-30 08:34:16 +0900

    #17 数値目標の2件目以降が登録できない問題の対応

--- app/controllers/api/v1/date_target_headers_controller.rb
+++ app/controllers/api/v1/date_target_headers_controller.rb
@@ -14,7 +14,7 @@ class Api::V1::DateTargetHeadersController < ApplicationController
     @qt.target_type = params[:target_type]
     @qt.quantity_kind = params[:quantity_kind]
     # TODO ソート順：いったん既存の最大+1を登録
-    temp_qt = QuantitativeTarget.find_by(user_id: current_user.id)
+    temp_qt = QuantitativeTarget.where(user_id: current_user.id)
     @qt.sort_order = 1 + (temp_qt.present? ? temp_qt.maximum('sort_order') : 1)
     @qt.default_zero_flg = params[:default_zero_flg]
     # TODO 項目色々足りない
